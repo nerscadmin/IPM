17b6cce53bd14fa77eb4394815d98519b3836465
swfrench <swfrench@lbl.gov>
2015-10-13 13:40:50 -0700

    Minor improvements to bootstrap.sh
    Now using a different search string for inserting fix into libtool.m4.

--- bootstrap.sh
+++ bootstrap.sh
@@ -34,21 +34,14 @@ fi
 autoreconf --install --force $warnings
 
 # check whether we can find the insertion point for our libtool.m4 bugfix
-if ! grep -q "Expand the sysroot to ease extracting the directories later" m4/libtool.m4; then
-	echo "error: could not find target key for patching libtool.m4 - please report this to the IPM developers"
+if ! grep -q "\-L\* | \-R\* | \-l\*)" m4/libtool.m4; then
+	echo "error: could not find target key for patching libtool.m4 - please report this to the IPM developers along with your m4/libtool.m4"
 	exit 1
 fi
 
 # apply the fix
 mv m4/libtool.m4 m4/libtool.m4_orig
-awk '/Expand the sysroot to ease extracting the directories later/ { printf("\
-       # Some compilers *also* place space between \"-l\" and the library name.\n\
-       # Remove the space.\n\
-       if test $p = \"-l\"; then\n\
-	 prev=$p\n\
-	 continue\n\
-       fi\n\n\
-"); } { print $0 }' m4/libtool.m4_orig > m4/libtool.m4
+awk '{ print $0 } /-L\* \| -R\* \| -l\*\)/ { printf("\n\t# Some compilers *also* place space between \"-l\" and the library name.\n\t# Remove the space.\n\tif test $p = \"-l\"; then prev=$p; continue; fi\n\n"); }' m4/libtool.m4_orig > m4/libtool.m4
 
 # autoreconf once more (no --force) to assimilate libtool.m4 changes
 autoreconf $warnings
