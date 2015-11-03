e0b8db248813986ba315ee5f62b75a4632f41d3d
swfrench <swfrench@lbl.gov>
2015-11-03 14:08:53 -0800

    Earlier edit to test.fhello always fails
    Call to pmpi_init_thread in lieu of mpi_init will always trigger
    segfault on mpi_finalize (pmpi_init_thread is not intercepted).

--- test/test.fhello/main.f
+++ test/test.fhello/main.f
@@ -1,13 +1,13 @@
       program hello
       implicit none
       integer size, rank, ierr
-      integer request, provide
+c      integer request, provide
       include 'mpif.h'
 
-      request = MPI_THREAD_SINGLE
+c      request = MPI_THREAD_SINGLE
 
-      call pmpi_init_thread(request, provide, ierr)
-c      call mpi_init(ierr)
+c      call pmpi_init_thread(request, provide, ierr)
+      call mpi_init(ierr)
       call mpi_comm_size(MPI_COMM_WORLD, size, ierr)
       call mpi_comm_rank(MPI_COMM_WORLD, rank, ierr)
       
